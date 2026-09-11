9175fb154fe2f4f3f28143b16ad5f69e397773d3
Dallan Simper <dallan.simper@gmail.com>
2026-09-11 15:04:33 -0600

    ya' basic loop

--- Game.Console/Program.cs
+++ Game.Console/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers = [ 1, 2, 3, 4, 5 ];
+
+        // for (int i = 0; i < numbers.Length; i++)
+        foreach(int number in numbers)
+        {
+            if (number % 2 != 0)
+            {
+                Console.Write(number + ", ");
+            }
+
+        }
+        
+
+
+    }
+}
