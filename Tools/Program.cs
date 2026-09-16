ee097e2678fa7d764d318e02d7c6c468047f58d3
Dallan Simper <dallan.simper@gmail.com>
2026-09-16 14:50:39 -0600

    array compare

--- Tools/Program.cs
+++ Tools/Program.cs
@@ -4,48 +4,47 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        int number = 7;
-        int[] arr = { 1, 2, 3, 4, 5 };
+        int[] arr1 = { 1, 2, 3 };
+        int[] arr2 = { 1, 2, 3 };
+        int[] arr3 = arr1;
 
-
-        Console.WriteLine("number: " + number);
-        DoesNothing(number);
-        Console.WriteLine("number: " + number);
-        DoesSomething(ref number);
-        Console.WriteLine("number: " + number);
-
-        for(int i=0; i < arr.Length; i++)
-        {
-            Console.Write(arr[i] + ", ");
-        }
-        Console.WriteLine(" ");
-        Multiply(arr);
-        for(int i=0; i < arr.Length; i++)
-        {
-            Console.Write(arr[i] + ", ");
-        }
-        Console.WriteLine(" ");
+        Console.WriteLine(ArrayCompare(arr1, arr2));
+        Console.WriteLine(ArrayCompare(arr1, arr3));
         
-    }
+        arr3 = new int[] {3,2,1};
 
-    static void Multiply(int[] list)
-    {
-        for(int i=0; i < list.Length; i++)
-        {
-            list[i] = list[i] * 2;
-        }
+        Console.WriteLine(ArrayCompare(arr1, arr3));
+
         
+
     }
 
-    static void DoesNothing(int val)
+    static bool ArrayCompare(int [] one, int [] two)
     {
-        val *= 2;
-        Console.WriteLine("val: " + val);
+        if (one.Length != two.Length)
+            return false;
+
+        for(int i = 0; i < one.Length; i++)
+        {
+            if(one[i] != two[i])
+            {
+                return false;
+            }    
+        }
+        return true;
     }
 
-    static void DoesSomething(ref int val)
+    static void ArrayAddressCompare(int [] one, int [] two)
     {
-        val *= 2;
-        Console.WriteLine("val: " + val);
+        if (one == two)
+        {
+            Console.WriteLine("They are the same.");
+        }
+        else
+        {
+            Console.WriteLine("They are NOT the same.");
+        }
     }
+
+
 }
