d097bbc91661577321a6c74a9424a2f484f74aa4
Dallan Simper <dallan.simper@gmail.com>
2026-09-21 15:36:16 -0600

    semi colon

--- Tools/Program.cs
+++ Tools/Program.cs
@@ -4,48 +4,25 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        int[] arr1 = { 1, 2, 3 };
-        int[] arr2 = { 1, 2, 3 };
-        int[] arr3 = arr1;
 
-        ArrayAddressCompare(arr1, arr2);
-        ArrayAddressCompare(arr1, arr3);
-        
-        arr3 = new int[] {3,2,1};
 
-        ArrayAddressCompare(arr1, arr3);
-
-        
-        Console.ForegroundColor = ConsoleColor.White;
-        Environment.Exit(0);
-    }
-
-    static bool ArrayCompare(int [] one, int [] two)
-    {
-        if (one.Length != two.Length)
-            return false;
-
-        for(int i = 0; i < one.Length; i++)
+        int num = 70;
+        if (num  < 10)
         {
-            if(one[i] != two[i])
-            {
-                return false;
-            }    
+            Console.WriteLine("shmall");
         }
-        return true;
-    }
 
-    static void ArrayAddressCompare(int [] one, int [] two)
-    {
-        if (one == two)
+
+
+
+        int i = 0;
+        // while ( i < 10)
+        for (;;)
         {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("They are the same.");
-        }
-        else
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("They are NOT the same.");
+            Console.Write(i + ", ");
+            i++;
+            if (i >= 10)
+                break;
         }
     }
 
